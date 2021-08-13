c1c53240da2cac4237c7fbd00f7105bc1f3acbb9
Michael Robards <mikerobards@icloud.com>
2021-08-13 11:35:58 -0400

    build initial elements on App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 function App() {
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Test your Typing Speed!</h1>
+      <textarea
+        name="textarea"
+        value=""
+        onChange={() => { }}
+        placeholder="start typing here!"
+      />
+      <h4>Time Remaining: </h4>
+      <button>Start!</button>
+      <h1>Word Count: </h1>
+
     </div>
   );
 }
 
 export default App;
+
+
