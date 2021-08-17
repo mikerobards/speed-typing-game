2af0fbc5673317777159a767991fce0263b6ff6a
Michael Robards <mikerobards@icloud.com>
2021-08-17 17:14:13 -0400

    convert business logic to hook

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
 import React, { useState, useEffect, useRef } from "react"
-
+import useChange from "./useChange";
 function App() {
-  const STARTING_TIME = 5
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isRunning, setIsRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textboxRef = useRef(null)
-
-  const handleChange = (e) => {
-    const { value } = e.target
-    setText(value)
-  }
-
-  const startGame = () => {
-    setIsRunning(true)
-    setTimeRemaining(STARTING_TIME)
-    setText("")
-    setWordCount(0)
-    textboxRef.current.disabled = false
-    textboxRef.current.focus()
-  }
-
-  const endGame = () => {
-    setIsRunning(false)
-    setWordCount(wordCounter(text))
-  }
-
-  const wordCounter = (str) => {
-    const wordsArr = str.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  }
-
-  useEffect(() => {
-    if (timeRemaining > 0 && isRunning) {
-      setTimeout(() => {
-        setTimeRemaining(time => time - 1)
-      }, 1000)
-    } else if (timeRemaining === 0) {
-      endGame()
-    }
-  }, [timeRemaining, isRunning])
-
+  const { handleChange, text, timeRemaining, isRunning, wordCount, startGame, textBoxRef } = useChange()
 
   return (
     <div>
@@ -52,7 +12,7 @@ function App() {
         onChange={handleChange}
         placeholder="start typing here!"
         disabled={!isRunning}
-        ref={textboxRef}
+        ref={textBoxRef}
       />
       <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={startGame} disabled={isRunning}>Start!</button>
